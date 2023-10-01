62e7b088fee4f52b715b2fdcbf6f04845fc3f327
JulianoAlfredo <juliano.alfredo86@gmail.com>
2023-10-01 15:33:05 -0300

    RETURN UNIX TIMESTAMP

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api/:date", (req, res) =>{
+  //exemplo 2023-10-01
+  var data = req.params.date
+  var date = new Date(data);
+  var unixTimestamp= Math.floor(date.getTime()/1000);
+
+  return res.json({
+    unix: unixTimestamp
+  })
+
+})
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
